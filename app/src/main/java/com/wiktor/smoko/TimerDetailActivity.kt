c8d8bddeac2dd6dea31d37252ef413b1d5408abd
wiktor <wiktor@bk.ru>
2022-08-30 22:52:47 +0300

    Add database

---
+++ app/src/main/java/com/wiktor/smoko/TimerDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.wiktor.smoko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimerDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_detail)
+    }
+}
